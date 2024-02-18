a96450b21a5c6cf3d4408047e19b6e5419c7821d
fabipereira <fabio.bpereira@mercadolivre.com>
2024-02-18 11:24:57 -0300

    Add code

---
+++ application/services/video_service_test.go
@@ -0,0 +1,57 @@
+package services_test
+
+import (
+	"codeflix-encoder/application/repositories"
+	"codeflix-encoder/application/services"
+	"codeflix-encoder/domain"
+	"codeflix-encoder/framework/database"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepositoryDb) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "videoId-8e5f3c9ddc1549b498d5f9caf7ea275c/type-VIDEO"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+
+	return video, repo
+}
+
+func TestVideoServiceDownload(t *testing.T) {
+
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("fc_catalog_admin")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+	err = videoService.Encode()
+	require.Nil(t, err)
+
+	err = videoService.Finish()
+	require.Nil(t, err)
+}
